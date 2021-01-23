2d9512739354c488b9dadb515d86e58cd094d35b
Kira Syromiatnikova <pereroyal@yandex.ru>
2021-01-23 23:23:21 +0300

    Bonus100

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int bonus;
+        int balance = 100;
+        int refill = 800;
+        int limit = 1000;
+                if (refill >= limit) {
+          bonus = refill/100;
+        } else {
+                    bonus =0;
+                }
+        int total = balance + refill + bonus;
+        System.out.println(total);
+    }
+}
